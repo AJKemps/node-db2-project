ab05fbd6d4ef0e86791c1216a6ee7a62a7c6669e
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-11 16:32:17 -0400

    get and post requests are working, mvp complete

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,16 +1,43 @@
 const express = require("express");
 
+const db = require("../cars/cars-config.js");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
   // do stuff here
-  res.status(200).json({ message: "cars router is running" });
+
+  db.select("*")
+    .from("cars")
+    .then((response) => {
+      res
+        .status(200)
+        .json({ message: "cars router is running", data: response });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "there was an issue connecting to the database" });
+    });
 });
 
 router.get("/:id", (req, res) => {
   // do stuff here
 });
 router.post("/", (req, res) => {
+  let newCar = req.body;
+
+  db("cars")
+    .insert(newCar)
+    .returning("id")
+    .then((response) => {
+      res.status(201).json({ inserted: response });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ message: "there was an issue connecting to the database" });
+    });
   // do stuff here
 });
 router.put("/:id", (req, res) => {
